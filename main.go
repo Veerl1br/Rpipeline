f35e7da1f862782ecd78456e36fefa91a8e4deba
Anton <dhevveprogramming@gmail.com>
2025-08-19 15:37:57 +0300

    added a few new function to the pipeline

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"sync"
@@ -10,23 +11,28 @@ import (
 
 // TODO: add timeout on http request
 // TODO: add logger to application
-// TODO: add new functions to the pipeline
-// TODO: add collection of query execution time metrics
+// TODO: add metrics function to the pipeline
+
+type SecurityReport struct {
+	HeadersSecure bool
+	TLSCertValid  bool
+}
 
 type Result struct {
-	res int
-	err error
+	res       *http.Response
+	secreport SecurityReport
+	err       error
 }
 
 func fetch(url string) Result {
-	time.Sleep(2 * time.Second) // long work emission
+	//time.Sleep(2 * time.Second) // long work emission
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return Result{0, err}
+		return Result{res: resp, err: err}
 	}
-
-	return Result{resp.StatusCode, err}
+	fmt.Println(url)
+	return Result{res: resp, err: err}
 }
 
 func generator(ctx context.Context, urls ...string) chan Result {
@@ -62,16 +68,43 @@ func generator(ctx context.Context, urls ...string) chan Result {
 	return result
 }
 
-func adder(ctx context.Context, ch chan Result) chan Result {
+func checkSecureHeader(headers http.Header) bool {
+	return headers.Get("Content-Security-Policy") != ""
+}
+
+// Вспомогательные функции проверки безопасности
+func checkCertValid(tls *tls.ConnectionState) bool {
+	if tls == nil {
+		return false
+	}
+	return len(tls.PeerCertificates) > 0 &&
+		time.Now().Before(tls.PeerCertificates[0].NotAfter)
+}
+
+func checkSecurity(ctx context.Context, ch chan Result) chan Result {
 	result := make(chan Result)
 
 	go func() {
 		defer close(result)
-		for v := range ch {
+
+		for {
 			select {
 			case <-ctx.Done():
 				return
-			case result <- Result{v.res + 100, v.err}:
+			case v, ok := <-ch:
+				if !ok {
+					return
+				}
+				if v.res == nil { // I dont know if I can do that
+					v.secreport = SecurityReport{false, false}
+					result <- v
+					continue
+				}
+				h := checkSecureHeader(v.res.Header)
+				t := checkCertValid(v.res.TLS)
+				v.secreport = SecurityReport{HeadersSecure: h, TLSCertValid: t}
+
+				result <- v
 			}
 		}
 	}()
@@ -80,11 +113,11 @@ func adder(ctx context.Context, ch chan Result) chan Result {
 }
 
 func main() {
-	urls := []string{"https://google.com", "https://httpstat.us/400", "https://google.com", "https://httpstat.us/400", "https://google.com", "https://httpstat.us/400"}
+	urls := []string{"https://google.com", "https://httpstat.us/400", "https://youtube.com", "https://twitch.tv"}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	for v := range adder(ctx, generator(ctx, urls...)) {
+	for v := range checkSecurity(ctx, generator(ctx, urls...)) {
 		if v.err != nil {
 			fmt.Println(v.err.Error())
 			continue
